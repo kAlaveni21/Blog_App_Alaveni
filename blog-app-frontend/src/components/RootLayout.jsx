14f8440c99e460a79bc79216a0afc9b32105ce95
kAlaveni21 <24eg105q44@anurag.edu.in>
2026-05-14 15:35:06 +0530

    Update RootLayout.jsx

--- blog-app-frontend/src/components/RootLayout.jsx
+++ blog-app-frontend/src/components/RootLayout.jsx
@@ -1,30 +1,30 @@
 import Header from "./Header";
 import Footer from "./Footer";
 import { Outlet } from "react-router";
-import {useEffect} from 'react'
 import { useAuth } from "../store/authStore";
-
-
-
+import { useEffect } from "react";
 function RootLayout() {
 
-  //import check checkAuth
-  let checkAuth=useAuth(state=>state.checkAuth)
+  const checkAuth = useAuth((state) => state.checkAuth);
+  const loading = useAuth((state) => state.loading);
 
-  useEffect(()=>{
-    checkAuth()
-  },[])
+  useEffect(() => {
+    checkAuth(); 
+  }, [checkAuth]);
 
+  if (loading) {
+    return <p>Loading app...</p>;
+  }
 
   return (
     <div>
       <Header />
-        <div className="min-h-screen mx-32">
-          <Outlet />
-        </div>
+      <div className='min-h-screen mx-32'>
+        <Outlet />
+      </div>
       <Footer />
     </div>
-  );
+  )
 }
 
 export default RootLayout;
