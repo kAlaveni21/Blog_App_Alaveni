99b89561726dde121e668a0819c2f5eaa111c3cf
kAlaveni21 <24eg105q44@anurag.edu.in>
2026-05-15 15:25:19 +0530

    Update main.jsx

--- blog-app-frontend/src/main.jsx
+++ blog-app-frontend/src/main.jsx
@@ -6,7 +6,7 @@ import axios from 'axios'
 
 // Set base URL for API calls
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
-axios.defaults.baseURL = "https://q44-backend.onrender.com";
+axios.defaults.baseURL = "https://two4eg105q08-capstone.onrender.com";
 axios.defaults.withCredentials = true;
 
 createRoot(document.getElementById('root')).render(
